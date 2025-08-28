9310e79664634c3d3eaef0062f64be0a2e95542c
herve02 <twubaherve@gmail.com>
2025-08-28 14:47:43 +0200

    comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
+        {/*  */}
       </head>
       <body className="font-sans antialiased scroll-smooth">{children}</body>
     </html>
